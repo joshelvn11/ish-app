134f5850917ee30edb38bfbaaef8f1ecbf5ef46e
Josh Beaver <josh.elvn11@gmail.com>
2024-06-19 12:24:11 +0100

    [FEAT] created basic login function

--- src/context/AuthContext.jsx
+++ src/context/AuthContext.jsx
@@ -1,4 +1,5 @@
 import React, { createContext, useState } from "react";
+import jwtDecode from "jwt-decode";
 
 const AuthContext = React.createContext();
 
@@ -8,8 +9,26 @@ export const AuthProvider = ({ children }) => {
   let [user, setUser] = useState(null);
   let [authTokens, setAuthTokens] = useState(null);
 
-  let loginUser = async (e) => {
+  let loginUser = async (username, password) => {
     e.preventDefault();
+    const respone = await fetch("http://127.0.0.1:8000/api/token/", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ username: username, password: password }),
+    });
+
+    let data = await response.json();
+
+    if (data) {
+      localStorage.setItem("authTokens", JSON.stringify(data));
+      setAuthTokens(data);
+      setUser(jwtDecode(data.access));
+      navigate("/");
+    } else {
+      alert("Something went wrong while loggin in the user!");
+    }
   };
 
   let logoutUser = (e) => {
