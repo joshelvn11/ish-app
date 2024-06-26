486652d95ea8cd3e60e15ae6bdfbed23b7006d63
Josh Beaver <josh.elvn11@gmail.com>
2024-06-26 16:10:18 +0100

    [IMPROV] conditionally rendered header and nav bar based on login state

--- src/components/core/Header.jsx
+++ src/components/core/Header.jsx
@@ -3,11 +3,11 @@ import { Link } from "react-router-dom";
 import AuthContext from "@/context/AuthContext";
 import ProfileWidget from "./ProfileWidget";
 import { Button } from "@/components/ui/button";
-import { MenuIcon } from "lucide-react";
+import { CloudDownload, MenuIcon } from "lucide-react";
 import UiContext from "@/context/UiContext";
 
 function Header() {
-  const { profile } = useContext(AuthContext);
+  const { profile, user } = useContext(AuthContext);
   const { setNavbarOpen } = useContext(UiContext);
 
   const toggleMenu = () => {
@@ -15,25 +15,27 @@ function Header() {
     setNavbarOpen((prevNavbarOpen) => !prevNavbarOpen);
   };
 
-  return (
-    <div className="flex justify-center w-full border-b">
-      <div className="flex items-center justify-between w-full p-3 md:px-9">
-        <div className="flex items-center h-full gap-3">
-          <Button variant="ghost" className="md:hidden" onClick={toggleMenu}>
-            <MenuIcon />
-          </Button>
-          <h1 className="font-playwrite">ish.</h1>
+  if (user) {
+    return (
+      <div className="flex justify-center w-full border-b">
+        <div className="flex items-center justify-between w-full p-3 md:px-9">
+          <div className="flex items-center h-full gap-3">
+            <Button variant="ghost" className="md:hidden" onClick={toggleMenu}>
+              <MenuIcon />
+            </Button>
+            <h1 className="font-playwrite">ish.</h1>
+          </div>
+          <nav className="flex items-center gap-5">
+            {profile ? (
+              <ProfileWidget className="mr-3" />
+            ) : (
+              <Link to="/login">Login</Link>
+            )}
+          </nav>
         </div>
-        <nav className="flex items-center gap-5">
-          {profile ? (
-            <ProfileWidget className="mr-3" />
-          ) : (
-            <Link to="/login">Login</Link>
-          )}
-        </nav>
       </div>
-    </div>
-  );
+    );
+  }
 }
 
 export default Header;
--- src/components/core/Navbar.jsx
+++ src/components/core/Navbar.jsx
@@ -4,59 +4,63 @@ import { Link } from "react-router-dom";
 import { ClipboardList, Home, Dumbbell } from "lucide-react";
 import { useLocation } from "react-router-dom";
 import UiContext from "@/context/UiContext";
+import AuthContext from "@/context/AuthContext";
 
 function Navbar() {
   const location = useLocation();
   const [path, setPath] = useState(location.pathname);
   const { navbarOpen, setNavbarOpen } = useContext(UiContext);
+  const { user } = useContext(AuthContext);
 
   useEffect(() => {
     setPath(location.pathname); // Update the path state whenever the location changes
     window.innerWidth < 768 && setNavbarOpen(false); // Close the nav bar on mobile devices
   }, [location]);
 
-  return (
-    <div
-      className={`${
-        navbarOpen ? "" : "hidden"
-      } h-full px-1 py-3 border-r w-full md:w-48 fixed md:relative bg-background `}
-    >
-      <div className="flex-1">
-        <nav className="grid items-start px-2 text-sm font-medium lg:px-4">
-          <Link
-            to="/"
-            onClick={() => setPath(location.pathname)}
-            className={`flex items-center gap-3 px-3 py-2 transition-all rounded-lg text-primary hover:text-primary ${
-              path == "/" ? "bg-muted" : null
-            }`}
-          >
-            <Home className="w-4 h-4" />
-            Dashboard
-          </Link>
-          <Link
-            to="/backlog"
-            onClick={() => setPath(location.pathname)}
-            className={`flex items-center gap-3 px-3 py-2 transition-all rounded-lg text-primary hover:text-primary ${
-              path == "/backlog" ? "bg-muted" : null
-            }`}
-          >
-            <ClipboardList className="w-4 h-4" />
-            Backlog{" "}
-          </Link>
-          <Link
-            to="/sprints"
-            onClick={() => setPath(location.pathname)}
-            className={`flex items-center gap-3 px-3 py-2 transition-all rounded-lg text-primary hover:text-primary ${
-              path == "/sprints" ? "bg-muted" : null
-            }`}
-          >
-            <Dumbbell className="w-4 h-4" />
-            Sprints{" "}
-          </Link>
-        </nav>
+  if (user) {
+    return (
+      <div
+        className={`${
+          navbarOpen ? "" : "hidden"
+        } h-full px-1 py-3 border-r w-full md:w-48 fixed md:relative bg-background `}
+      >
+        <div className="flex-1">
+          <nav className="grid items-start px-2 text-sm font-medium lg:px-4">
+            <Link
+              to="/"
+              onClick={() => setPath(location.pathname)}
+              className={`flex items-center gap-3 px-3 py-2 transition-all rounded-lg text-primary hover:text-primary ${
+                path == "/" ? "bg-muted" : null
+              }`}
+            >
+              <Home className="w-4 h-4" />
+              Dashboard
+            </Link>
+            <Link
+              to="/backlog"
+              onClick={() => setPath(location.pathname)}
+              className={`flex items-center gap-3 px-3 py-2 transition-all rounded-lg text-primary hover:text-primary ${
+                path == "/backlog" ? "bg-muted" : null
+              }`}
+            >
+              <ClipboardList className="w-4 h-4" />
+              Backlog{" "}
+            </Link>
+            <Link
+              to="/sprints"
+              onClick={() => setPath(location.pathname)}
+              className={`flex items-center gap-3 px-3 py-2 transition-all rounded-lg text-primary hover:text-primary ${
+                path == "/sprints" ? "bg-muted" : null
+              }`}
+            >
+              <Dumbbell className="w-4 h-4" />
+              Sprints{" "}
+            </Link>
+          </nav>
+        </div>
       </div>
-    </div>
-  );
+    );
+  }
 }
 
 export default Navbar;
