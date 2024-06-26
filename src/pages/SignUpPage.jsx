7677cf3f15b1c673f7a744700722b9779e657552
Josh Beaver <josh.elvn11@gmail.com>
2024-06-26 15:52:42 +0100

    [FEAT] added sign up page

---
+++ src/pages/SignUpPage.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import SignUpForm from "@/components/authentication/SignUpForm";
+
+function SignUpPage() {
+  return (
+    <div className="flex items-center justify-center w-full h-full">
+      <SignUpForm />
+    </div>
+  );
+}
+
+export default SignUpPage;
