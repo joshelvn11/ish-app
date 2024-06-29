cfd56db6610a8e83bfa2ca81935d141f6b881843
Josh Beaver <josh.elvn11@gmail.com>
2024-06-29 17:49:55 +0100

    [FEAT] created create item dialog

--- src/components/project-management/ItemCreateSelect.jsx
+++ src/components/project-management/ItemCreateSelect.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import {
   Select,
   SelectContent,
@@ -6,6 +6,16 @@ import {
   SelectTrigger,
   SelectValue,
 } from "@/components/ui/select";
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+  DialogFooter,
+} from "@/components/ui/dialog";
+import { Button } from "@/components/ui/button";
 import { PlusIcon } from "@radix-ui/react-icons";
 
 const createItem = (value) => {
@@ -13,17 +23,51 @@ const createItem = (value) => {
 };
 
 function ItemCreateSelect() {
+  const [isDialogOpen, setIsDialogOpen] = useState(false);
+  const [itemType, setItemType] = useState(null);
+
+  const handleValueChange = (value) => {
+    setIsDialogOpen(true);
+    setItemType(value);
+  };
+
+  const closeDialog = () => {
+    setIsDialogOpen(false);
+  };
+
   return (
-    <Select value={""} onValueChange={createItem}>
-      <SelectTrigger className="w-[120px] sm:w-[150px]" icon={PlusIcon}>
-        <SelectValue placeholder="Create" />
-      </SelectTrigger>
-      <SelectContent>
-        <SelectItem value={0}>Epic</SelectItem>
-        <SelectItem value={1}>User Story</SelectItem>
-        <SelectItem value={2}>Task</SelectItem>
-      </SelectContent>
-    </Select>
+    <div>
+      <Select value={""} onValueChange={handleValueChange}>
+        <SelectTrigger className="w-[120px] sm:w-[150px]" icon={PlusIcon}>
+          <SelectValue placeholder="Create" />
+        </SelectTrigger>
+        <SelectContent>
+          <SelectItem value="EPIC">Epic</SelectItem>
+          <SelectItem value="USERSTORY">User Story</SelectItem>
+          <SelectItem value="TASK">Task</SelectItem>
+        </SelectContent>
+      </Select>
+
+      <Dialog open={isDialogOpen} onOpenChange={setIsDialogOpen}>
+        <DialogContent>
+          <DialogHeader>
+            <DialogTitle>
+              Create new {itemType === "EPIC" && <span>Epic</span>}{" "}
+              {itemType === "USERSTORY" && <span>User Story</span>}{" "}
+              {itemType === "TASK" && <span>Task</span>}
+            </DialogTitle>
+            <DialogDescription>
+              {itemType === "EPIC" && <p>Creating epic</p>}
+            </DialogDescription>
+          </DialogHeader>
+          <DialogFooter>
+            <Button type="submit" onClick={closeDialog}>
+              Confirm
+            </Button>
+          </DialogFooter>
+        </DialogContent>
+      </Dialog>
+    </div>
   );
 }
 
