c955e14d61d4817b38d3ddea0bcc2aa76d78d9c7
Josh Beaver <josh.elvn11@gmail.com>
2024-06-30 17:10:54 +0100

    [FEAT] added epic status and priority badges to epic card

--- src/components/project-management/EpicCard.jsx
+++ src/components/project-management/EpicCard.jsx
@@ -23,6 +23,7 @@ import {
   DialogTrigger,
   DialogFooter,
 } from "@/components/ui/dialog";
+import { Badge } from "@/components/ui/badge";
 import TaskTable from "./TaskTable";
 import { EnterFullScreenIcon, TrashIcon } from "@radix-ui/react-icons";
 import EpicForm from "./forms/EpicForm";
@@ -55,6 +56,34 @@ function EpicCard(props) {
               </CollapsibleTrigger>
             </div>
           </div>
+          {(props.status || props.priority) && (
+            <div id="badge-wrapper" className="flex gap-2 pb-2">
+              {props.priority === "OPTIONAL" && (
+                <Badge className="bg-blue-500">OPTIONAL</Badge>
+              )}
+              {props.priority === "BENEFICIAL" && (
+                <Badge className="bg-emerald-600">BENEFICIAL</Badge>
+              )}
+              {props.priority === "ESSENTIAL" && (
+                <Badge className="bg-amber-500">ESSENTIAL</Badge>
+              )}
+              {props.priority === "CRITICAL" && (
+                <Badge className="bg-red-600">CRITICAL</Badge>
+              )}
+              {props.status === "TO DO" && (
+                <Badge className="bg-red-600">TO DO</Badge>
+              )}
+              {props.status === "IN PROGRESS" && (
+                <Badge className="bg-amber-500">IN PROGRESS</Badge>
+              )}
+              {props.status === "REVIEW" && (
+                <Badge className="bg-blue-500">REVIEW</Badge>
+              )}
+              {props.status === "DONE" && (
+                <Badge className="bg-emerald-600">DONE</Badge>
+              )}
+            </div>
+          )}
           <p className="pb-3 text-sm">{props.description}</p>
           <CollapsibleContent className="space-y-2">
             <TaskTable groupBy={"EPIC"} groupId={props.epicId}></TaskTable>
