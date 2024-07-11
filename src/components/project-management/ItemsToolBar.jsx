37868ee5da4f36bbb49d5a31cfbfd69f071a04b9
Josh Beaver <josh.elvn11@gmail.com>
2024-07-11 22:10:55 +0100

    [FEAT] created sory by component

--- src/components/project-management/ItemsToolBar.jsx
+++ src/components/project-management/ItemsToolBar.jsx
@@ -1,10 +1,13 @@
 import React from "react";
 import ItemCreateSelect from "./ItemCreateSelect";
+import StatusSort from "./filter-sort/StatusSort";
 
 function ItemsToolBar() {
   return (
     <div className="sticky top-0 z-10 flex items-center justify-between w-full p-3 border-b bg-background">
-      <div>Filter & Sorting</div>
+      <div>
+        <StatusSort />
+      </div>
       <div>
         <ItemCreateSelect />
       </div>
--- src/components/project-management/filter-sort/StatusSort.jsx
+++ src/components/project-management/filter-sort/StatusSort.jsx
@@ -8,9 +8,26 @@ import {
   SelectTrigger,
   SelectValue,
 } from "@/components/ui/select";
+import { ArrowDownWideNarrow } from "lucide-react";
 
 function StatusSort() {
-  return <div>StatusSort</div>;
+  return (
+    <Select>
+      <SelectTrigger className="w-[180px]">
+        <ArrowDownWideNarrow className="size-5" />
+        <SelectValue placeholder="Sort by" />
+      </SelectTrigger>
+      <SelectContent>
+        <SelectGroup>
+          <SelectLabel>Options</SelectLabel>
+          <SelectItem value="STATUS">Status</SelectItem>
+          <SelectItem value="PRIORITY">Priority</SelectItem>
+          <SelectItem value="DUEDATE">Due Date</SelectItem>
+          <SelectItem value="TYPE">Type</SelectItem>
+        </SelectGroup>
+      </SelectContent>
+    </Select>
+  );
 }
 
 export default StatusSort;
