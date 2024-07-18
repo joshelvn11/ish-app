6652e0f58c48c40beaa2e42fbb6bdd85f9bb9777
Josh Beaver <josh.elvn11@gmail.com>
2024-07-18 22:55:05 +0100

    [FIX] removed buggy code from previous commits

--- src/components/project-management/ItemsToolBar.jsx
+++ src/components/project-management/ItemsToolBar.jsx
@@ -5,41 +5,10 @@ import ItemFilter from "@/components/project-management/filter-sort/ItemFilter";
 function ItemsToolBar(props) {
   const [isScrolling, setIsScrolling] = useState(false);
 
-  useEffect(() => {
-    const handleTouchStart = () => {
-      clearTimeout(isScrolling);
-    };
-
-    const handleTouchMove = () => {
-      setIsScrolling(true);
-    };
-
-    const handleTouchEnd = () => {
-      setIsScrolling(
-        setTimeout(() => {
-          setIsScrolling(false);
-        }, 100)
-      );
-    };
-
-    const scrollableDiv = document.getElementById("items-toolbar");
-    scrollableDiv.addEventListener("touchstart", handleTouchStart);
-    scrollableDiv.addEventListener("touchmove", handleTouchMove);
-    scrollableDiv.addEventListener("touchend", handleTouchEnd);
-
-    return () => {
-      scrollableDiv.removeEventListener("touchstart", handleTouchStart);
-      scrollableDiv.removeEventListener("touchmove", handleTouchMove);
-      scrollableDiv.removeEventListener("touchend", handleTouchEnd);
-    };
-  }, [isScrolling]);
-
   return (
     <div
       id="items-toolbar"
-      className={`sticky top-0 z-10 flex gap-2 items-center justify-between w-full p-3 border-b bg-background overflow-x-scroll overflow-y-hidden  ${
-        isScrolling ? "pointer-events-none" : ""
-      }`}
+      className="sticky top-0 z-10 flex gap-2 items-center justify-between w-full p-3 border-b bg-background overflow-x-scroll overflow-y-hidden"
     >
       <div className="flex gap-2">
         <ItemFilter
