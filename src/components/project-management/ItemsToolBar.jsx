59789075e97e0a5eddb7ff20cdf5029f537dd89b
Josh Beaver <josh.elvn11@gmail.com>
2024-07-18 10:51:13 +0100

    [FIX] fixed button clicks on item toolbar scroll

--- src/components/project-management/ItemsToolBar.jsx
+++ src/components/project-management/ItemsToolBar.jsx
@@ -3,8 +3,42 @@ import ItemCreateSelect from "@/components/project-management/ItemCreateSelect";
 import ItemFilter from "@/components/project-management/filter-sort/ItemFilter";
 
 function ItemsToolBar(props) {
+  const [isScrolling, setIsScrolling] = useState(false);
+
+  useEffect(() => {
+    const handleTouchStart = () => {
+      clearTimeout(isScrolling);
+    };
+
+    const handleTouchMove = () => {
+      setIsScrolling(true);
+    };
+
+    const handleTouchEnd = () => {
+      setIsScrolling(
+        setTimeout(() => {
+          setIsScrolling(false);
+        }, 200)
+      );
+    };
+
+    const scrollableDiv = document.getElementById("items-toolbar");
+    scrollableDiv.addEventListener("touchstart", handleTouchStart);
+    scrollableDiv.addEventListener("touchmove", handleTouchMove);
+    scrollableDiv.addEventListener("touchend", handleTouchEnd);
+
+    return () => {
+      scrollableDiv.removeEventListener("touchstart", handleTouchStart);
+      scrollableDiv.removeEventListener("touchmove", handleTouchMove);
+      scrollableDiv.removeEventListener("touchend", handleTouchEnd);
+    };
+  }, [isScrolling]);
+
   return (
-    <div className="sticky top-0 z-10 flex gap-2 items-center justify-between w-full p-3 border-b bg-background overflow-x-scroll overflow-y-hidden">
+    <div
+      id="items-toolbar"
+      className="sticky top-0 z-10 flex gap-2 items-center justify-between w-full p-3 border-b bg-background overflow-x-scroll overflow-y-hidden"
+    >
       <div className="flex gap-2">
         <ItemFilter
           filterOptions={props.filterOptions}
